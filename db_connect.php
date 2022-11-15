51083cbe3f09b06d9acb16594e2303b8c8b7213a
Warorua Alex <45052494+Warorua@users.noreply.github.com>
2022-11-15 08:57:10 +0300

    FIRST GIT COMMIT

---
+++ db_connect.php
@@ -0,0 +1,5 @@
+<?php 
+
+//$conn= new mysqli('localhost','root','','voting_db')or die("Could not connect to mysql".mysqli_error($con));
+
+$conn= new mysqli('localhost','tsavosit_collo','Hmb2SykZ-.k]','tsavosit_collo')or die("Could not connect to mysql".mysqli_error($con));
